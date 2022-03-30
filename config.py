e9feeaa47766522e032e1c226aa38b26c3e327c3
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2022-03-30 06:20:11 +0300

    Edit config.py Added function uploads_list().

---
+++ config.py
@@ -0,0 +1,16 @@
+from ntpath import join
+import os
+
+TOKEN = 'Enter You Token'
+
+BASE_PATH = os.getcwd()
+UPLOADS_DIR = 'uploads'
+
+
+def uploads_list():
+    list_dir = os.listdir(UPLOADS_DIR)
+    list = []
+    for item in list_dir:
+        result = os.path.join(BASE_PATH, UPLOADS_DIR, item)
+        list.append(result)
+    return list
